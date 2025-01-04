a0e59ad30c641a42ca02cef207ea8942b9fc381f
Priyanka <priyanka@kartikengitech.com>
2025-01-04 11:45:43 +0530

    user

--- src/app/modules/user/models/user.model.js
+++ src/app/modules/user/models/user.model.js
@@ -72,9 +72,55 @@ const UserSchema = new mongoose.Schema(
     isAcceptTermConditions: { type: Boolean, default: false },
     isDeleted: { type: Boolean, default: false },
     isDeactivated: { type: Boolean, default: false },
+    // for seller
+    shopName: { type: String, trim: true },
+    ownerName: { type: String, trim: true },
+    businessAddress: {
+      street: { type: String, trim: true },
+      city: { type: String, trim: true },
+      state: { type: String, trim: true },
+      pinCode: { type: String, trim: true },
+      country: { type: String, trim: true },
+    },
+    businessScale: {
+      type: String,
+      enum: ["small", "medium", "large"], //Small Business (Local)
+      //Medium Business (City-wide)
+      //Large Business (Multiple Cities/State-wide)
+      default: "small",
+    },
+    isDeliveryAvailable: {
+      type: Boolean,
+      default: false,
+    },
+    contactNumber: { type: String, trim: true },
+    contactEmail: { type: String, trim: true },
+    description: { type: String, trim: true },
+    facebookUrl: { type: String, trim: true },
+    instagramUrl: { type: String, trim: true },
+    youtubeUrl: { type: String, trim: true },
+    websiteUrl: { type: String, trim: true },
   },
   { timestamps: true }
 );
 
 const User = mongoose.model("User", UserSchema);
 module.exports = { User };
+
+// urpose: Allow sellers to receive relevant product requests.
+// Fields in Seller Profile:
+// 1. Shop Name
+// 2. Owner Name
+// 3. Contact Number
+// 4. Email Address
+// 5. Business Address
+// 6. Business Scale (optional)
+// Small Business (Local)
+// • Medium Business (City-wide)
+// • Large Business (Multiple Cities/State-wide)
+// • Delivery Available: Yes/No toggle (optional)
+// • description
+// 7. Profile Picture (optional).
+// 8. Social media(optional)
+// • Location: Seller’s shop location (GPS or manual)
+// • when profile is done add “more categories option”
