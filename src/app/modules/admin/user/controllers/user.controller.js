a484a814b6d097c18b2b79e98e81e215e1d2a8ba
Priyanka <priyanka@kartikengitech.com>
2025-01-06 18:58:15 +0530

    delete, deactivate profile

--- src/app/modules/admin/user/controllers/user.controller.js
+++ src/app/modules/admin/user/controllers/user.controller.js
@@ -1,4 +1,13 @@
 const { allUsers, getUserById } = require("../bussiness/user.bussiness");
 
+const {
+  deleteProfile,
+  deactivateProfile,
+} = require("../../../user/bussiness/user.bussiness");
+
 exports.allUsers = async (req) => await allUsers(req.user, req.query);
 exports.getUserById = async (req) => await getUserById(req.user, req.query);
+exports.deleteProfile = async (req) =>
+  await deleteProfile({ _id: req.query.id, id: req.query.id });
+exports.deactivateProfile = async (req) =>
+  await deactivateProfile({ _id: req.query.id, id: req.query.id });
--- src/app/modules/admin/user/routes/user.route.js
+++ src/app/modules/admin/user/routes/user.route.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express();
 const { wrapAsync } = require("../../../../helpers/router.helper");
 const { adminAuthenticate } = require("../../../../middleware/jwt.middleware");
-const { allUsers, getUserById } = require("../controllers/user.controller");
+const {
+  allUsers,
+  getUserById,
+  deleteProfile,
+  deactivateProfile,
+} = require("../controllers/user.controller");
 
 router.get("/allUsers", adminAuthenticate, wrapAsync(allUsers));
 router.get("/getUserById", adminAuthenticate, wrapAsync(getUserById));
+router.delete("/deleteProfile", adminAuthenticate, wrapAsync(deleteProfile));
+router.delete(
+  "/deactivateProfile",
+  adminAuthenticate,
+  wrapAsync(deactivateProfile)
+);
 
 module.exports = router;
