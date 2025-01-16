30c976cce4d279c64c9486115bd0f0c9fc62cd9f
Priyanka <priyanka@kartikengitech.com>
2025-01-16 18:59:05 +0530

    rating

--- src/app/modules/seller/rating/bussiness/rating.bussiness.js
+++ src/app/modules/seller/rating/bussiness/rating.bussiness.js
@@ -5,13 +5,17 @@ const { Rating } = require("../models/rating.model");
 const rating = async (user, query, body) => {
   if (!isValid(query.postId)) throw "postId must be a valid";
   body.userId = user._id;
+  if (!isValid(body.rate) || body.rate < 1 || body.rate > 5)
+    throw "rate must be between 1 and 5";
   const rate = await Rating.updateOne(
     { postId: query.postId },
     { $push: { rating: body } },
     { upsert: true }
   );
+
   return {
     msg: msg.success,
+    data: rate,
   };
 };
 
